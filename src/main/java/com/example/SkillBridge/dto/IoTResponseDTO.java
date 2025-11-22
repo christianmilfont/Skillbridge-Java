2f7c8cbf42e41086c7db98b2365af0da92716adb
Christian Milfont <cmilfont021@gmail.com>
2025-11-21 21:19:59 -0300

    adicionando atributo nome em iotreponse

--- src/main/java/com/example/SkillBridge/dto/IoTResponseDTO.java
+++ src/main/java/com/example/SkillBridge/dto/IoTResponseDTO.java
@@ -1,9 +1,13 @@
 package com.example.SkillBridge.dto;
 
-import lombok.Data;import java.util.List;
-@Data public class IoTResponseDTO {
+import lombok.Data;
+import java.util.List;
+@Data 
+public class IoTResponseDTO {
 
     private Long id;
+    private String nome;                    
+
     private MelhorVagaDTO melhor_vaga;
     private List<VagaCompatibilidadeDTO> todas_as_vagas;
 }
